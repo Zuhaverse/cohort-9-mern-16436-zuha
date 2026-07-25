451a481f0daeb7dbdf97572778871c02b5b2f80b
Zuha <zuhawahabz@gmail.com>
2026-07-25 19:14:35 +0500

    Configure MySQL database connection

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const loggerMiddleware = require("./middleware/loggerMiddleware");
+const logger = require("./logger/logger.js");
+const db = require("./config/db");
 
 require("dotenv").config();
 const express = require('express');
@@ -10,6 +12,19 @@ app.use(loggerMiddleware);
 app.get('/', (req, res) => {
     res.send('Backend is running');
 });
+
+async function connectDatabase() {
+  try {
+    const connection = await db.getConnection();
+    logger.info("Database connected successfully");
+    connection.release();
+  } catch (error) {
+    logger.error(error, "Database connection failed");
+    process.exit(1);
+  }
+}
+
+connectDatabase();
 
 app.listen(port, () => {
     const logger = require("./logger/logger");
